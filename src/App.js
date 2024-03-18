f0fd7d8d08870114bc9f5efb7f220611308f5f20
Oles K. <lessenover@gmail.com>
2024-03-18 21:39:19 +0200

    Add main tic tac toe logic.

--- src/App.js
+++ src/App.js
@@ -15,16 +15,35 @@ function Square({ value, onSquareClick }) {
 }
 
 function TicTacToeBoard() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const columnsCount = 3;
 
   function onSquareClick(idx) {
+    if (squares[idx] || calculateWinner(squares)) {
+      return;
+    }
     const newSquares = squares.slice();
-    newSquares[idx] = idx;
+    if (xIsNext) {
+      newSquares[idx] = "X";
+    } else {
+      newSquares[idx] = "O";
+    }
     setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
   return (
     <>
+      <p>{status}</p>
       {Array(3)
         .fill("")
         .map((el, rowIdx) => (
@@ -32,10 +51,12 @@ function TicTacToeBoard() {
             key={rowIdx}
             className="board-row"
           >
-            {Array(3)
+            {Array(columnsCount)
               .fill("")
               .map((el, colIdx) => {
-                const squareIdx = rowIdx * 3 + colIdx;
+                /*Since there are 3 columns in each row, multiplying the rowIndex by 3 
+                gives the starting index of the current row in the squares array.*/
+                const squareIdx = rowIdx * columnsCount + colIdx;
                 return (
                   <Square
                     key={squareIdx}
@@ -48,6 +69,26 @@ function TicTacToeBoard() {
         ))}
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+      return squares[a];
+  }
+  return null;
 }
 
 function App() {
